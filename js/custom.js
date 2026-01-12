e419e4cad9ec1d6a8d3bf7ff19068fe721f44991
kamilbartko1 <kamil.bartko1@gmail.com>
2026-01-12 21:34:31 +0100

    Update paths to relative and restore vercel.json configuration

--- js/custom.js
+++ js/custom.js
@@ -18,11 +18,11 @@
     // ABOUT SLIDER
     $('body').vegas({
         slides: [
-            { src: '/images/slide-image01.jpg' },
-            { src: '/images/slide-image02.jpg' },
-            { src: '/images/slide-image03.jpg' },
-            { src: '/images/slide-image04.jpg' },
-            { src: '/images/slide-image05.jpg' }
+            { src: 'images/slide-image01.jpg' },
+            { src: 'images/slide-image02.jpg' },
+            { src: 'images/slide-image03.jpg' },
+            { src: 'images/slide-image04.jpg' },
+            { src: 'images/slide-image05.jpg' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
